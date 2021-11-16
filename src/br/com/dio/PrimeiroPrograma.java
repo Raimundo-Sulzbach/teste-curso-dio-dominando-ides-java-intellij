bbf63306159b01897a9007e159393d6dd5af2121
Raimundo-Sulzbach <Raimundo.Sulzbach@gmail.com>
2021-11-16 20:20:01 +0100

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+import br.com.dio.model.Gato;
+
+import static java.lang.System.out;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+//        int a = 5;
+//        int b = 3;
+//        System.out.println("Hello world!" + (a+b));
+        Gato gato = new Gato();
+        System.out.println(gato);
+    }
+}
